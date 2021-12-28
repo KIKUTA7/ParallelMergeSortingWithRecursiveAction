ff843f830e4b2916f1efc92f090c127612090756
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-12-28 13:30:13 +0400

    jndsckndskc

--- src/pgdp/threads/BetterParallelMergeSort.java
+++ src/pgdp/threads/BetterParallelMergeSort.java
@@ -11,32 +11,73 @@ public class BetterParallelMergeSort extends RecursiveAction {
     @Override
     protected void compute() {
         if(array.length >= 15000) {
+            if(array.length % 4 == 0) {
+                if(array.length < 4) {MergeSort.mergesort(array);return;}
+                int quarter = array.length/4;
+                Comparable [] array1 = new Comparable[quarter];
+                Comparable [] array2 = new Comparable[quarter];
+                Comparable [] array3 = new Comparable[quarter];
+                Comparable [] array4 = new Comparable[quarter];
+
+                for (int i=0;i<= quarter - 1;i++)
+                    array1[i] = array[i];
+                for (int i= quarter;i<= 2*quarter - 1;i++)
+                    array2[i - quarter] = array[i];
+                for (int i=2*quarter;i<= 3*quarter - 1;i++)
+                    array3[i - 2*quarter] = array[i];
+                for (int i=3*quarter;i<= 4*quarter - 1;i++)
+                    array4[i - 3*quarter] = array[i];
+
+                BetterParallelMergeSort par1 = new BetterParallelMergeSort(array1);
+                BetterParallelMergeSort par2 = new BetterParallelMergeSort(array2);
+                BetterParallelMergeSort par3 = new BetterParallelMergeSort(array3);
+                BetterParallelMergeSort par4 = new BetterParallelMergeSort(array4);
+
+                invokeAll(par1,par2,par3,par4);
+
+                for (int i=0;i<= quarter- 1;i++)
+                    array[i] = array1[i] ;
+                for (int i= quarter;i<= 2*quarter - 1;i++)
+                    array[i] = array2[i - quarter];
+                for (int i=2*quarter;i<= 3*quarter - 1;i++)
+                    array[i] = array3[i - 2*quarter];
+                for (int i=3*quarter;i<= 4*quarter - 1;i++)
+                    array[i] = array4[i - 3*quarter];
+
+                MergeSort.merge(array,new Comparable[2*quarter],0,quarter - 1,2*quarter - 1);
+                MergeSort.merge(array,new Comparable[array.length],2*quarter,
+                        3*quarter - 1, 4*quarter - 1);
+
+                MergeSort.merge(array,new Comparable[array.length],0,2*quarter - 1 ,4*quarter - 1); }
+            else
+            {
+                if(array.length < 2) {return;}
+
+                Comparable [] array1 = new Comparable[array.length/2];
+                Comparable [] array2 = new Comparable[array.length - array.length/2];
 
 
-        Comparable [] array1 = new Comparable[array.length/2];
-        Comparable [] array2 = new Comparable[array.length - array.length/2];
+                for (int i=0;i<= array.length/2 - 1;i++)
+                    array1[i] = array[i];
+                for (int i= array.length/2;i<= array.length - 1;i++)
+                    array2[i - array.length/2] = array[i];
 
 
-        for (int i=0;i<= array.length/2 - 1;i++)
-            array1[i] = array[i];
-        for (int i= array.length/2;i<= array.length - 1;i++)
-            array2[i - array.length/2] = array[i];
+                BetterParallelMergeSort par1 = new BetterParallelMergeSort(array1);
+                BetterParallelMergeSort par2 = new BetterParallelMergeSort(array2);
 
 
-        BetterParallelMergeSort par1 = new BetterParallelMergeSort(array1);
-        BetterParallelMergeSort par2 = new BetterParallelMergeSort(array2);
+                invokeAll(par1,par2);
 
-
-        invokeAll(par1,par2);
-
-        for (int i=0;i<= array.length/2 - 1;i++)
-            array[i] = array1[i] ;
-        for (int i= array.length/2;i<= array.length - 1;i++)
-            array[i] = array2[i - array.length/2];
+                for (int i=0;i<= array.length/2 - 1;i++)
+                    array[i] = array1[i] ;
+                for (int i= array.length/2;i<= array.length - 1;i++)
+                    array[i] = array2[i - array.length/2];
 
 
 
-        MergeSort.merge(array,new Comparable[array.length],0,array.length/2 - 1,array.length - 1);}
+                MergeSort.merge(array,new Comparable[array.length],0,array.length/2 - 1,array.length - 1);
+            }}
         else
         {
             MergeSort.mergesort(array);
--- src/pgdp/threads/ParallelMergeSort.java
+++ src/pgdp/threads/ParallelMergeSort.java
@@ -11,46 +11,46 @@ public class ParallelMergeSort extends RecursiveAction {
 
     @Override
     protected void compute() {
-//        if(array.length % 4 == 0) {
-//        if(array.length < 4) {MergeSort.mergesort(array);return;}
-//        int quarter = array.length/4;
-//        Comparable [] array1 = new Comparable[quarter];
-//        Comparable [] array2 = new Comparable[quarter];
-//        Comparable [] array3 = new Comparable[quarter];
-//        Comparable [] array4 = new Comparable[array.length - 3* quarter];
-//
-//        for (int i=0;i<= quarter - 1;i++)
-//            array1[i] = array[i];
-//        for (int i= quarter;i<= 2*quarter - 1;i++)
-//            array2[i - quarter] = array[i];
-//        for (int i=2*quarter;i<= 3*quarter - 1;i++)
-//            array3[i - 2*quarter] = array[i];
-//        for (int i=3*quarter;i<= array.length - 1;i++)
-//            array4[i - 3*quarter] = array[i];
-//
-//        ParallelMergeSort par1 = new ParallelMergeSort(array1);
-//        ParallelMergeSort par2 = new ParallelMergeSort(array2);
-//        ParallelMergeSort par3 = new ParallelMergeSort(array3);
-//        ParallelMergeSort par4 = new ParallelMergeSort(array4);
-//
-//        invokeAll(par1,par2,par3,par4);
-//
-//        for (int i=0;i<= quarter- 1;i++)
-//            array[i] = array1[i] ;
-//        for (int i= quarter;i<= 2*quarter - 1;i++)
-//            array[i] = array2[i - quarter];
-//        for (int i=2*quarter;i<= 3*quarter - 1;i++)
-//            array[i] = array3[i - 2*quarter];
-//        for (int i=3*quarter;i<= array.length - 1;i++)
-//            array[i] = array4[i - 3*quarter];
-//
-//        MergeSort.merge(array,new Comparable[2*quarter],0,(2*quarter - 1)/2,2*quarter - 1);
-//        MergeSort.merge(array,new Comparable[array.length - 2*quarter + 7],2* quarter,
-//                (2*quarter + array.length - 1)/2, array.length - 1);
-//
-//        MergeSort.merge(array,new Comparable[array.length],0,2*quarter - 1 ,array.length - 1); }
-//        else
-//        {
+        if(array.length % 4 == 0) {
+        if(array.length < 4) {MergeSort.mergesort(array);return;}
+        int quarter = array.length/4;
+        Comparable [] array1 = new Comparable[quarter];
+        Comparable [] array2 = new Comparable[quarter];
+        Comparable [] array3 = new Comparable[quarter];
+        Comparable [] array4 = new Comparable[quarter];
+
+        for (int i=0;i<= quarter - 1;i++)
+            array1[i] = array[i];
+        for (int i= quarter;i<= 2*quarter - 1;i++)
+            array2[i - quarter] = array[i];
+        for (int i=2*quarter;i<= 3*quarter - 1;i++)
+            array3[i - 2*quarter] = array[i];
+        for (int i=3*quarter;i<= 4*quarter - 1;i++)
+            array4[i - 3*quarter] = array[i];
+
+        ParallelMergeSort par1 = new ParallelMergeSort(array1);
+        ParallelMergeSort par2 = new ParallelMergeSort(array2);
+        ParallelMergeSort par3 = new ParallelMergeSort(array3);
+        ParallelMergeSort par4 = new ParallelMergeSort(array4);
+
+        invokeAll(par1,par2,par3,par4);
+
+        for (int i=0;i<= quarter- 1;i++)
+            array[i] = array1[i] ;
+        for (int i= quarter;i<= 2*quarter - 1;i++)
+            array[i] = array2[i - quarter];
+        for (int i=2*quarter;i<= 3*quarter - 1;i++)
+            array[i] = array3[i - 2*quarter];
+        for (int i=3*quarter;i<= 4*quarter - 1;i++)
+            array[i] = array4[i - 3*quarter];
+
+        MergeSort.merge(array,new Comparable[2*quarter],0,quarter - 1,2*quarter - 1);
+        MergeSort.merge(array,new Comparable[array.length],2*quarter,
+                3*quarter - 1, 4*quarter - 1);
+
+        MergeSort.merge(array,new Comparable[array.length],0,2*quarter - 1 ,4*quarter - 1); }
+        else
+        {
                     if(array.length < 2) {return;}
 
                     Comparable [] array1 = new Comparable[array.length/2];
@@ -77,7 +77,7 @@ public class ParallelMergeSort extends RecursiveAction {
 
 
                     MergeSort.merge(array,new Comparable[array.length],0,array.length/2 - 1,array.length - 1);
-               // }
+                }
 
 
 
@@ -87,7 +87,7 @@ public class ParallelMergeSort extends RecursiveAction {
     }
 
     public static void main(String[] args) {
-        Integer hah [] = new Integer[] {3,7,1,9,8,5,10,1,4,7,8,9,16,4,6};
+        Integer hah [] = new Integer[] {3,7,2,1,5,3,7,8,9,5,3,4,27,10,8};
         new ParallelMergeSort(hah).compute();
         for (int i = 0; i < hah.length; i++) {
             System.out.print(hah[i] + " " );
